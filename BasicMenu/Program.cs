6dd89853ef6695bac442c109c895883222f1b5c8
Ted-Singleton <tedsingleton6@gmail.com>
2024-03-07 14:42:38 +0000

    Created User class to hold a user's username, password and other details. Changed hardcoded strings in user dictionary to use instances of this new class. Added basic functionality for menu options, using details from instances of the class

--- BasicMenu/Program.cs
+++ BasicMenu/Program.cs
@@ -1,4 +1,5 @@
-﻿internal class Program
+﻿using BasicMenu;
+internal class Program
 {
     //We need to keep asking for the username and password until an input is received
     //We will use this a few times, so we break it out into a function
@@ -24,14 +25,14 @@
     }
 
     //We need to check the username and password, so we can break this out into a function.
-    //We pass in the dictionary of correct login details, rather than create it here, since we may want to use it in other functions later
-    private static bool CheckPassword(string un, string pw, Dictionary<string, string> details)
+    //We pass in the dictionary of users, rather than create it here, since we may want to use it in other functions later
+    private static bool CheckPassword(string un, string pw, Dictionary<string, User> details)
     {
         //check if the input username is in the dictionary in a case-insensitive way
         if(details.ContainsKey(un.ToLower()))
         {
             //if it is, check if the password matches and return that value
-            return details[un.ToLower()] == pw;
+            return details[un.ToLower()].Password == pw;
         }
         else
         {
@@ -44,13 +45,20 @@
     {
         //Initialise our correct login details
         //EDIT: Made this a dictionary so we can handle multiple users
-        Dictionary<string, string> correctLoginDetails = new Dictionary<string, string>
+
+        //first we instantiate our users
+        User user1 = new User("ted", "Password1","Ted", "tedsingleton6@gmail.com");
+        User user2 = new User("trevor", "Password2", "Trevor", "trevor.smith@peregrineresourcing.com");
+        User user3 = new User("alice", "Password3");
+        User user4 = new User("bob", "Password4");
+
+        //and then we add them to a dictionary
+        Dictionary<string, User> users = new Dictionary<string, User>
         {
-            //I changed the stored usernames to lower case, so we can still perform a case-insensitive check
-            {"ted", "Password42!"},
-            {"trevor", "Password123!"},
-            {"alice", "Password!"},
-            {"bob", "Bassword!"}
+            {user1.Username, user1},
+            {user2.Username, user2},
+            {user3.Username, user3},
+            {user4.Username, user4}
         };
 
         //Initialise our input variables
@@ -62,33 +70,37 @@
 
         //EDIT: created a boolean to store the result of the login check
         //This isn't optimal, but I find it makes the code more readable than having the CheckPassword function call directly in the if statement
-        bool loginSuccess = CheckPassword(username, password, correctLoginDetails);
+        bool loginSuccess = CheckPassword(username, password, users);
 
         //Now we can check the username and password
         //If they're correct, we can display the menu
         if (loginSuccess)
         {
-            //EDIT: moved these inside the if statement, since we only need them if the user successfully logs in
-            //We can use a string to store our menu
-            string menu = $"""
-            Welcome, {username}!
-            --Main Menu--
-                1. Create new member
-                2. Edit member
-                3. View member
-                4. Delete member
-                X. Exit
-            """;
+            //We can use the appropriate user from the dictionary for their details
+            User currentUser = users[username.ToLower()];
 
+            //EDIT: moved these inside the if statement, since we only need them if the user successfully logs in
             //We also need a bool to keep the program running
             bool running = true;
 
             //We now initialise our selection variable
             string selection;
+
             //and start our main loop
             //EDIT: I changed this to a do-while loop, since we want the menu to be displayed at least once. We save some time by not having to check the condition before the loop starts
             do
             {
+                //We can use a string to store our menu
+                //EDIT: Since we're now allowing the user to edit their display name, we have to bring that into the loop so it can update
+                string menu = $"""
+                Welcome, {currentUser.Name}!
+                --Main Menu--
+                    1. Create new member
+                    2. Edit member
+                    3. View member
+                    4. Delete member
+                    X. Exit
+                """;
                 //Display the menu
                 Console.WriteLine(menu);
 
@@ -99,22 +111,61 @@
                 switch (selection)
                 {
                     case "1":
+                        //here, we ask for details to create a new user
+                        //the minimum requirement is a username and password
                         Console.WriteLine("Creating new member...");
+                        string newUsername = HandleInput("Please enter a username: ");
+                        string newPassword = HandleInput("Please enter a password: ");
+                        //we could also ask for a name and email, but we'll leave them as default for now
+                        users.Add(newUsername, new User(newUsername, newPassword));
                         break;
+
                     case "2":
+                        //we can edit the current user's details
+                        //in future, this could be its own breakout menu, allowing the user to select what to edit about their account
+                        //for now, we'll just allow them to change their display name
                         Console.WriteLine("Editing member...");
+                        currentUser.Name = HandleInput("Please enter a new display name: ");
                         break;
+
                     case "3":
-                        Console.WriteLine("Viewing member...");
+                        //we can view the current user's details
+                        Console.WriteLine("Here are your details:");
+                        Console.WriteLine($"Username: {currentUser.Username}");
+                        Console.WriteLine($"Display Name: {currentUser.Name}");
+                        Console.WriteLine($"Email: {currentUser.Email}");
                         break;
+
                     case "4":
-                        Console.WriteLine("Deleting member...");
+                        //here, we can delete a user
+                        Console.WriteLine("Please select a member to delete:");
+                        //we show the usernames of all the users
+                        foreach (var user in users)
+                        {
+                            Console.WriteLine(user.Key);
+                        }
+                        //and then ask for the username of the user to delete. we also make it lowercase to match the dictionary
+                        string deleteUsername = HandleInput("Please enter the username of the member you want to delete: ").ToLower();
+
+                        //if the user exists, we can remove them
+                        if(users.ContainsKey(deleteUsername))
+                        {
+                            users.Remove(deleteUsername);
+                            Console.WriteLine("Member deleted.");
+                        }
+                        //if not, we tell the user that it failed
+                        else
+                        {
+                            Console.WriteLine("Member not found.");
+                        }
                         break;
+
                     case "x":
                         Console.WriteLine("Exiting...");
                         //If the user selects X, we can set running to false, and the program will exit
                         running = false;
                         break;
+
                     default:
                         //This will show if the user enters something, but not a valid option
                         Console.WriteLine("Invalid selection, please try again");
---
+++ BasicMenu/User.cs
@@ -0,0 +1,35 @@
+﻿namespace BasicMenu
+{
+    //We want a User class to store the user's details, beyond just username and password
+    internal class User
+    {
+        //We can use properties to store the user's details
+        //For now, we'll just store the username, password, name and email with public getters and private setters.
+        //Once we get onto developing the menu's functionality, we'll need to be more specific about these access modifiers depending on how we want to implement menu options.
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public string Name { get; set; }
+        public string Email { get; private set; }
+
+        //our default constructor requires a username and password. We can set the name and email later
+        public User(string username, string password)
+        {
+            //We force all usernames to be lower case, so we can compare them easily
+            Username = username.ToLower();
+            Password = password;
+            //We can set the name to the username by default
+            Name = Username;
+            //And the email to "Not set" by default
+            Email = "Not set";
+        }
+
+        //We can also create a constructor that sets the name and email at the same time
+        public User(string username, string password, string name, string email)
+        {
+            Username = username.ToLower();
+            Password = password;
+            Name = name;
+            Email = email;
+        }
+    }
+}
